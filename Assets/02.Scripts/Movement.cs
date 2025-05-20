0400d92d2ee8cec660a1a922bf16fd7458470912
sangjin0213 <sangjin370@gmail.com>
2025-05-20 11:11:01 +0900

    [ADD]Unity Project

---
+++ Assets/02.Scripts/Movement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed;
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        }
+
+    }
+}
